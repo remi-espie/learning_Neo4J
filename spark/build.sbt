47477d8b85cb00faf4178d19e54b265a44725c02
remi <remi.espie@etu.umontpellier.fr>
2025-03-26 17:18:02 +0100

    fix: mongo

--- spark/build.sbt
+++ spark/build.sbt
@@ -4,14 +4,14 @@ version := "1.0"
 
 scalaVersion := "2.12.18"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.5"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.1"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.5.5",
   "org.apache.hadoop" % "hadoop-common" % "3.3.1",
   "org.apache.hadoop" % "hadoop-aws" % "3.3.1",
   "org.neo4j" % "neo4j-connector-apache-spark_2.12" % "5.3.5_for_spark_3",
-  "org.mongodb.spark" %% "mongo-spark-connector" % "10.4.1",
+  "org.mongodb.spark" % "mongo-spark-connector_2.12" % "10.4.1",
 )
 
 assemblyMergeStrategy in assembly := {
--- spark/src/main/scala/MongoDB.scala
+++ spark/src/main/scala/MongoDB.scala
@@ -4,22 +4,23 @@ object MongoDB {
   def main(args: Array[String]): Unit = {
 
     // Replace with the actual connection URI and credentials
-    val uri = "mongodb://localhost:27017"
+    val username = "root"
+    val password = "root"
     val database = "database"
     val collection = "collection"
+    val uri = s"mongodb://$username:$password@localhost:27017/$database?authSource=admin"
 
     val spark = SparkSession.builder
       .appName("MongoDB")
       .master("local[*]")
-      .config("spark.mongodb.input.uri", s"$uri/$database.$collection")
-      .config("spark.mongodb.output.uri", s"$uri/$database.$collection")
+      .config("spark.mongodb.read.connection.uri", s"$uri")
+      .config("spark.mongodb.write.connection.uri", s"$uri")
       .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
 
     val df = spark.read
       .format("mongodb")
-      .option("database", database)
       .option("collection", collection)
       .load()
 
