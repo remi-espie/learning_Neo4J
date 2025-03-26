e9e7e367e650c62ecc0b2083c647b12586393d59
remi <remi.espie@etu.umontpellier.fr>
2025-03-26 11:59:45 +0100

    wip

--- spark/build.sbt
+++ spark/build.sbt
@@ -5,3 +5,9 @@ version := "1.0"
 scalaVersion := "2.12.18"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.5.5",
+  "org.apache.hadoop" % "hadoop-common" % "3.4.1",
+  "org.apache.hadoop" % "hadoop-aws" % "3.4.1",
+)
--- spark/src/main/scala/UserTest.scala
+++ spark/src/main/scala/UserTest.scala
@@ -3,17 +3,26 @@ import org.apache.spark.sql.functions._
 
 object UserTest {
   def main(args: Array[String]): Unit = {
+    val minioEndpoint = sys.env("MINIO_ENDPOINT")
+    val minioAccessKey = sys.env("MINIO_ACCESS_KEY")
+    val minioSecretKey = sys.env("MINIO_SECRET_KEY")
+    val minioBucket = sys.env("MINIO_BUCKET")
+    val minioFile = sys.env("MINIO_FILE")
 
     val spark = SparkSession.builder()
       .appName("UsersTest")
       .master("local[*]")
+      .config("spark.hadoop.fs.s3a.endpoint", minioEndpoint)
+      .config("spark.hadoop.fs.s3a.access.key", minioAccessKey)
+      .config("spark.hadoop.fs.s3a.secret.key", minioSecretKey)
+      .config("spark.hadoop.fs.s3a.path.style.access", "true")
       .getOrCreate()
 
     import spark.implicits._
     val usersDF = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
-      .csv("./users.csv")
+      .csv(s"s3a://$minioBucket/$minioFile")
 
     val result = usersDF
       .filter($"age" >= 25)
